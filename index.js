0598d4062017768f5b3b1e8fe78181c9aa343e38
roshan kumar <roshankumar40786@gmail.com>
2022-09-11 23:52:22 +0530

    user

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var header = document.getElementById("parent");
-var btns = header.getElementsByClassName("btns");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function myfun () {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-        // current.className = el.className === 'active' ? 'inactive' : 'active';
-    })
-};
+// var header = document.getElementById("parent");
+// var btns = header.getElementsByClassName("btns");
+// for (var i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function myfun () {
+//         var current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//         // current.className = el.className === 'active' ? 'inactive' : 'active';
+//     })
+// };
 
